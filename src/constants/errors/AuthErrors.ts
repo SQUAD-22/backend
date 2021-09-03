054e52d0f826a1c81f3120ed13f1a04f0cd3fcfb
vinicius <84585585+hellovinicius@users.noreply.github.com>
2021-09-03 11:20:41 -0300

    feat: add auth errors constants

---
+++ src/constants/errors/AuthErrors.ts
@@ -0,0 +1,33 @@
+export default {
+  INVALID_EMAIL: {
+    status: 400,
+    errorId: 'AUTH/INVALID_EMAIL',
+    message: 'O e-mail fornecido é inválido.',
+  },
+  MISSING_FIELDS: {
+    status: 400,
+    errorId: 'AUTH/MISSING_FIELDS',
+    message:
+      'Algum ou alguns dos parâmetros obrigatórios não foram enviados corretamente.',
+  },
+  PASSWORD_TOO_SHORT: {
+    status: 400,
+    errorId: 'AUTH/PASSWORD_TOO_SHORT',
+    message: 'A senha deve conter no mínimo 6 caracteres.',
+  },
+  EMAIL_NOT_FOUND: {
+    status: 404,
+    errorId: 'AUTH/EMAIL_NOT_FOUND',
+    message: 'O e-mail informado não está cadastrado.',
+  },
+  WRONG_PASSWORD: {
+    status: 409,
+    errorId: 'AUTH/WRONG_PASSWORD',
+    message: 'A senha informada não coincide com o cadastro.',
+  },
+  EMAIL_NOT_VERIFIED: {
+    status: 401,
+    errorId: 'AUTH/EMAIL_NOT_VERIFIED',
+    message: 'O e-mail deste usuário ainda não foi verificado.',
+  },
+};
