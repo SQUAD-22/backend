b21fc778de1556f3cad5f888d44737d7d66227a3
vinicius <84585585+hellovinicius@users.noreply.github.com>
2021-09-03 15:22:43 -0300

    refactor: check if user exists in validator

--- src/validators/Auth.validators.ts
+++ src/validators/Auth.validators.ts
@@ -1,14 +1,19 @@
+import UserService from '../services/database/User.service';
 import AuthErrors from '../constants/errors/AuthErrors';
 
 export default {
-  register: (email: string) => {
+  register: async (email: string) => {
     //Verificar se os campos foram passados corretamente
     if (!email) return AuthErrors.MISSING_FIELDS;
 
-    //Validar email
+    //Valida email
     const emailRegex =
       /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(fcamara)\.com.br$/;
     if (!emailRegex.test(email)) return AuthErrors.INVALID_EMAIL;
+
+    //Verifica se usuário já existe no banco de dados
+    const user = await UserService.findByEmail(email);
+    if (user) return AuthErrors.ACCOUNT_ALREADY_EXISTS;
 
     return false;
   },
