1d4eadb8d29cc93fb9f93b59911723b8cdac321f
vinicius <84585585+hellovinicius@users.noreply.github.com>
2021-09-03 15:22:06 -0300

    feat: create user service

---
+++ src/services/database/User.service.ts
@@ -0,0 +1,13 @@
+import UserModel from '../../models/User.model';
+
+export default {
+  findByEmail: async (email: string) => {
+    const user = await UserModel.findOne({ email: email });
+    return user;
+  },
+
+  createUserByEmail: async (email: string) => {
+    const user = await UserModel.create({ email });
+    return user;
+  },
+};
