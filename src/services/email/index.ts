683668bbbd8e874b591f10423787901649bad0df
Vinicius <oui.vinicius@gmail.com>
2021-09-04 22:43:48 +0000

    feat: implement email-templates

--- src/services/email/index.ts
+++ src/services/email/index.ts
@@ -1,8 +1,10 @@
 import { createTransport } from 'nodemailer';
+import Email from "email-templates"
 
 //Desestruturando as informações do .env
 const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
 
+//Criar um transporter para envio de emails
 let transporter = createTransport({
   host: SMTP_HOST,
   port: parseInt(SMTP_PORT),
@@ -12,4 +14,17 @@ let transporter = createTransport({
   },
 });
 
-export default transporter;
+//Criar uma instancia do email-templates para gerenciar templates
+const email = new Email({
+  transport: transporter,
+  send: true,
+  preview: false,
+  views: {
+    options: {
+      extension:"pug"
+    },
+    root: "./templates/"
+  }
+})
+
+export default email;
